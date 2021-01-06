053f297eb1309fd5ce7d1f1f19e57bc8db215648
Ivan Buetler <ivan.buetler@compass-security.com>
2021-01-06 12:23:06 +0100

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 docker build --no-cache -t hackinglab/alpine-openssh-server:3.2.0 -t hackinglab/alpine-openssh-server:3.2 -t hackinglab/alpine-openssh-server:latest -f Dockerfile .
 
 docker push hackinglab/alpine-openssh-server
+docker push hackinglab/alpine-openssh-server:3.2
+docker push hackinglab/alpine-openssh-server:3.2.0
